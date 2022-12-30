d4815080bb738b3dfd38f2958f11cbb7aad4a45f
wlavoy <wal60766@uga.edu>
2022-12-30 12:09:30 -0500

    simple density measurement is working *problem was startPos was not updating... fixed by adding startPos array into both loops*

--- 2D_model_traits_work/simple_density.py
+++ 2D_model_traits_work/simple_density.py
@@ -23,40 +23,58 @@ data = plt.imshow(img_array)
 
 
 #draw the plot
-plt.show()
+#plt.show()
 
 #determine how large the array is
 #print(img_array.shape)
 # 3456 by 5184
 
 #empty variables for walking
+j = 0
 i = 0
 root = 0
 background = 0
 
-#start position for walker
-startPos = np.array([0,i])
+#start position variable
+startPos = []
+
 
 
 #want to walk from top to bottom one column at a time
-for i in range(0,5184):
-    print("column number: " + str(i))
-    #steps and variable for walker
-    j = 0
-    step = [j,0]
-    #while loop to iterate through columns
-    while j < 3457:
-        #print("row number: " + str(j))
+for i in range(0,5183):
+    #start position updates in array
+    startPos = np.array([j,i])
+    i += 1
+    #print("column number: " + str(i))
+   
+    #while loop to iterate through rows
+    for j in range(0,3456):
+        #start position updates in array
+        startPos = np.array([j,i])
+        
+        #check to see if position contains a root and update root count
         if img_array[startPos[0], startPos[1]] == 1:
             root += 1
-            print("root count: " + str(root))
+            #print("root count: " + str(root))
             j += 1
-            startPos = startPos + step
-        elif img_array[startPos[0], startPos[1]] == 2:
+        
+        #if position is not a root it must be background
+        elif img_array[startPos[0], startPos[1]] != 1:
             background += 1
             #print("background count: " + str(background))
             j += 1
-            startPos = startPos + step
-    
+           
+    #reset j value after while loop so we start from the top position and work our way down
+    j = 0
+
+print("Total Root Count: " + str(root))
+print("Total Background Count: " +str(background))
+
+density = (root/background)
+print("Density of Roots/Background: ")
+print(density)
+
+
+
 
 
